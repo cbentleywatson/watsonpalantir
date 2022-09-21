79a8514c5133ef7e3146954bca6da6fcdd499f09
Connor Watson <cbentleywatson@gmail.com>
2022-09-20 20:16:42 -0400

    new tool scripts for upload and management

---
+++ tool-scripts/restart-prod.sh
@@ -0,0 +1,1 @@
+ssh -i ../../configs/second_instance_creation.pem ec2-user@ec2-54-89-197-93.compute-1.amazonaws.com  < restart-prod-commands.txt
---
+++ tool-scripts/upload-prod-yml.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sftp -b sftp-commands.txt -i ../../configs/second_instance_creation.pem ec2-user@ec2-54-89-197-93.compute-1.amazonaws.com
+
+#scp -P 23 /home/kalenpw/.m2/repository/com/Khalidor/TestPlugin/0.0.1-SNAPSHOT/TestPlugin-0.0.1-SNAPSHOT.jar kalenpw@184.155.136.254:/home/kalenpw/TestWorld/plugins
